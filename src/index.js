bdf7d22fbbbcc0ae630dcd8af7325b06ce3ef46c
Junaid Khan <junaid@nimapinfotech.com>
2023-08-06 17:27:27 +0530

    deploy

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
-    <RouterProvider router={appRouter} />
+    <RouterProvider basename="/tasklist" router={appRouter} />
   </Provider>
   // </React.StrictMode>
 );
